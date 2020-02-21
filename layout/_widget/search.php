a25ab7eaa295d01e5ed0dd2a5cae18d2301f8432
安和 <42889600+AHCorn@users.noreply.github.com>
2020-02-21 23:39:26 +0800

    增加搜索小部件

---
+++ layout/_widget/search.php
@@ -0,0 +1,27 @@
+<style>
+	#nexmoe-header .nexmoe-search input {
+    border: none!important;
+    background-color: transparent;
+    padding: 20px;
+    box-sizing: border-box;
+    outline: 0;
+}
+[type=search] {
+    -webkit-appearance: textfield;
+    outline-offset: -2px;
+}
+button, input {
+    overflow: visible;
+}
+button, input, optgroup, select, textarea {
+    margin: 0;
+    font-family: inherit;
+    font-size: 100%;
+    line-height: 1.15;
+}
+</style>
+<div class="nexmoe-widget-wrap">
+	<div class="nexmoe-widget nexmoe-search">
+	        <form class="search" method="post" action="./search" role="search">
+            <input type="search" placeholder="搜索" name="s" class="search-input" value="">
+            </form></div></div>
