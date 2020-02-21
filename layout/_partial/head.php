ad84bc249061f052c0253bd4e6719444d7237847
安和 <42889600+AHQAQ@users.noreply.github.com>
2020-02-21 23:10:13 +0800

    首页显示副标题

--- layout/_partial/head.php
+++ layout/_partial/head.php
@@ -2,7 +2,7 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <html>
     <head>
-        <title><?php $this->archiveTitle(array( 'category' => _t('分类 %s 下的文章'), 'search' => _t('包含关键字 %s 的文章'), 'tag' => _t('标签 %s 下的文章'), 'author' => _t('%s 发布的文章') ), '', ' - '); ?><?php $this->options->title(); ?></title>
+       <title><?php $this->archiveTitle(array( 'category' => _t('分类 %s 下的文章'), 'search' => _t('包含关键字 %s 的文章'), 'tag' => _t('标签 %s 下的文章'), 'author' => _t('%s 发布的文章') ), '', ' - '); ?><?php $this->options->title(); ?><?php if($this->is('index')): ?> - <?php $this->options->description() ?><?php endif; ?></title>
         <meta charset="<?php $this->options->charset(); ?>">
         <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
         <meta name="renderer" content="webkit">
