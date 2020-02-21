978289509bf3e66adf56a9b3214fd290f0bcc4c9
安和 <42889600+AHCorn@users.noreply.github.com>
2020-02-22 02:26:55 +0800

    挪动文章样式文件 提高首屏速度

--- layout/_partial/after-footer.php
+++ layout/_partial/after-footer.php
@@ -1,28 +1,9 @@
 <script src="<?php echo $this->options->mdui_js ?>"></script>
-<script src="<?php echo $this->options->jquery_js ?>"></script>
 
-<?php if (!empty($this->options->function) && in_array('fancybox', $this->options->function)): ?>
-<script src="<?php echo $this->options->fancybox_js ?>"></script>
-<?php endif; ?>
+
 
 <?php if (!empty($this->options->function) && in_array('SmoothScroll', $this->options->function)): ?>
 <script src="<?php echo $this->options->SmoothScroll_js ?>"></script>
 <?php endif; ?>
-<?php if (!empty($this->options->function) && in_array('enableMathjax', $this->options->function)): ?>
-    <?php if ($this->fields->math != "No"): ?>
-        <script type="text/x-mathjax-config">MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});</script>'
-        <script type="text/javascript" src="https://cdn.bootcss.com/mathjax/2.7.6/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
-    <?php endif; ?>
-<?php endif; ?>
-<?php if (empty($this->options->function) || !in_array('enableMathjax', $this->options->function)): ?>
-    <?php if ($this->fields->math == "Yes"): ?>
-        <script type="text/x-mathjax-config">MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});</script>'
-        <script type="text/javascript" src="https://cdn.bootcss.com/mathjax/2.7.6/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
-    <?php endif; ?>
-<?php endif; ?>
-
-<script src="<?php echo $this->options->highlight_js ?>"></script>
-<script>hljs.initHighlightingOnLoad();</script>
-
 <script src="<?php $this->options->themeUrl('source/js/app.js'); ?>"></script>
 <script src="<?php echo $this->options->lazysizes_js ?>"></script>
