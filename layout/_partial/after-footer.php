203065244fd0f60843dd8f8772fb6900f87be153
奇趣保罗 <dreamer_paul@126.com>
2020-04-16 12:47:02 +0800

    Update Footer
    修复无法食用 JS 插件的问题

--- layout/_partial/after-footer.php
+++ layout/_partial/after-footer.php
@@ -5,3 +5,4 @@
 <?php endif; ?>
 <script src="<?php $this->options->themeUrl('source/js/app.js'); ?>"></script>
 <script src="<?php echo $this->options->lazysizes_js ?>"></script>
+<?php $this -> footer(); ?>
