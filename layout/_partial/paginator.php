c9d9343cf409bf284199a36392f21e0899706f9f
折影轻梦 <nexmoe@qq.com>
2021-07-23 17:04:54 +0800

    #24

--- layout/_partial/paginator.php
+++ layout/_partial/paginator.php
@@ -1,1 +1,14 @@
-<?php $this->pageNav('<', '>', 2, '...', array('wrapTag' => 'nav', 'wrapClass' => 'nexmoe-page-nav', 'itemTag' => '', 'textTag' => 'span', 'currentClass' => 'page-number current', 'prevClass' => 'extend prev', 'nextClass' => 'extend next',)); ?>
+<?php 
+$this->pageNav(
+    '<','>', 2, '...', 
+     array(
+        'wrapTag' => 'nav',
+        'wrapClass' => 'nexmoe-page-nav', 
+        'itemTag' => '', 
+        'textTag' => 'a', 
+        'currentClass' => 'page-number current', 
+        'prevClass' => 'extend prev', 
+        'nextClass' => 'extend next',
+    )
+);
+?>
